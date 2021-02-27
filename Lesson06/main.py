68a9fc6ca57e90458fb1e8fc45411870cae32e60
Yaroslav <Lovtsov_Yaroslav@mail.ru>
2021-02-27 14:13:49 +0300

    Домашнее задание 7

--- Lesson06/main.py
+++ Lesson06/main.py
@@ -2,11 +2,12 @@ import os
 from dotenv import load_dotenv
 from scrapy.crawler import CrawlerProcess
 from scrapy.settings import Settings
-from instagram.spiders.insta import InstagramSpider
+from instagram.spiders.insta_followers import InstaFollowersSpider
 
 
 if __name__ == "__main__":
-    tags = ['китайскийязык', 'chinesecharacters', 'learnarabic']
+    # tags = ['китайскийязык', 'chinesecharacters', 'learnarabic']
+    user_name = '_oktaytan_'
     load_dotenv('.env')
 
     cookies_args = f'mid={os.getenv("MID")}; shbid={os.getenv("SHBID")}; hbts={os.getenv("SHBTS")};'
@@ -14,6 +15,8 @@ if __name__ == "__main__":
     crawler_settings = Settings()
     crawler_settings.setmodule("instagram.settings")
     crawler_process = CrawlerProcess(settings=crawler_settings)
-    crawler_process.crawl(InstagramSpider, login=os.getenv('INSTA_USERNAME'), password=os.getenv('INSTA_PASSWORD'),
-                          tags=tags, cookies_args=cookies_args)
+    # crawler_process.crawl(InstagramSpider, login=os.getenv('INSTA_USERNAME'), password=os.getenv('INSTA_PASSWORD'),
+    #                       tags=tags, cookies_args=cookies_args)
+    crawler_process.crawl(InstaFollowersSpider, login=os.getenv('INSTA_USERNAME'), password=os.getenv('INSTA_PASSWORD'),
+                           user_name=user_name, cookies_args=cookies_args)
     crawler_process.start()
